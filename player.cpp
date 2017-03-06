0e94fa9aed3a4a4f4909be047769ad5a25ebe592
Cherie Jia <cjia@caltech.edu>
2017-03-05 19:31:24 -0800

    changed players

--- player.cpp
+++ player.cpp
@@ -5,6 +5,8 @@
  * on (BLACK or WHITE) is passed in as "side". The constructor must finish
  * within 30 seconds.
  */
+
+/* Team Ruthwick and Sathwick */
 Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
